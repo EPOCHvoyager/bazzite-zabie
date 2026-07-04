29dfda8ce5c90c18bde38e2f3c37af83b73d9a44
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-07-04 02:49:29 -0300

    Enable Mullvad scriptlets to fix mullvad-exclude

--- build_files/packages/mullvad.sh
+++ build_files/packages/mullvad.sh
@@ -7,7 +7,6 @@ echo Installing Mullvad VPN software…
 dnf5 config-manager addrepo \
 	--from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
 dnf5 -y install \
-	--setopt=tsflags=noscripts \
 	mullvad-vpn
 dnf5 config-manager disable \
 	mullvad-stable
@@ -17,15 +16,3 @@ rpm -V \
     mullvad-vpn
 
 echo Successfully installed.
-
-
-echo Enabling service units…
-
-systemctl enable mullvad-daemon.service
-systemctl enable mullvad-early-boot-blocking.service
-
-
-systemctl is-enabled mullvad-daemon.service
-systemctl is-enabled mullvad-early-boot-blocking.service
-
-echo Successfully enabled.
