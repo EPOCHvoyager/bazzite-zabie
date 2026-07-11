5d2cbf81601e936c1381735510e4b7a570a12d8c
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-07-11 13:18:46 -0300

    Add step markers

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,15 +5,29 @@ set ${CI:+-x} -euo pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
+
 # Enable local service unit from system_files
+echo Enabling pci-latency service…
+
 systemctl enable pci-latency.service
+
+
 systemctl is-enabled pci-latency.service
+
+echo Successfully enabled.
+
+
+# Run all package installation scripts
+echo Installing packages…
 
 PACKAGE_DIR="/ctx/packages"
 
 for f in "${PACKAGE_DIR}"/*.sh; do
 	sh -c "$f" || exit 1
 done
+
+echo Package installation done.
+
 
 # Disable Krunner in favor of Vicinae
 echo Disabling Krunner…
