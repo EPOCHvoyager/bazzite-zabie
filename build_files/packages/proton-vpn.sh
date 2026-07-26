1b49a1ba18c94314e7ab6bc283c217f03b492600
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-07-26 13:05:42 -0300

    Simplify repository installation

--- build_files/packages/proton-vpn.sh
+++ build_files/packages/proton-vpn.sh
@@ -4,23 +4,20 @@ set ${CI:+-x} -euo pipefail
 
 echo Installing Proton VPN software…
 
-TMP_DIR="/tmp"
-RELEASE="$(rpm -E '%fedora')"
-
 _install_repo() {
-    local repo_rpm ; repo_rpm="protonvpn-stable-release-1.0.4-1.noarch.rpm"
-    local repo_rpm_url ; repo_rpm_url="https://repo.protonvpn.com/fedora-${RELEASE}-stable/protonvpn-stable-release/${repo_rpm}"
-
-    wget "${repo_rpm_url}" -P "${TMP_DIR}"
-
-    dnf5 -y install \
-        "${TMP_DIR}/${repo_rpm}"
-
-
-    rpm -V \
-        "$( rpm -qp --qf '%{NAME}\n' "${TMP_DIR}/${repo_rpm}" )"
-
-    rm "${TMP_DIR}"/*.rpm
+cat > /etc/yum.repos.d/protonvpn-stable.repo << EOF
+#
+# ProtonVPN stable release
+#
+[protonvpn-fedora-stable]
+name = ProtonVPN Fedora Stable repository
+baseurl = https://repo.protonvpn.com/fedora-$releasever-stable
+enabled = 1
+gpgcheck = 1
+repo_gpgcheck=0
+skip_if_unavailable=true
+gpgkey = https://repo.protonvpn.com/fedora-$releasever-stable/public_key.asc
+EOF
 }
 
 _install_repo
