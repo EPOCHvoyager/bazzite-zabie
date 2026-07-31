92db51a2668a6b91eff4b796853519de9762c586
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-07-31 17:31:52 -0300

    Add fix for Plasma Integration

--- build_files/packages/librewolf.sh
+++ build_files/packages/librewolf.sh
@@ -20,3 +20,16 @@ rpm -V \
 dnf5 repolist --disabled | grep -q "${REPO_ID}"
 
 echo Successfully installed.
+
+
+echo Applying fix for Plasma Integration…
+
+# See — https://www.librewolf.net/docs/faq/#how-do-i-get-native-messaging-to-work.
+# The Fedora plasma-browser-integration package puts this in /lib64, LibreWolf looks for it in /lib.
+mkdir /usr/lib/librewolf
+ln -s /usr/lib64/librewolf/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
+
+
+[[ -f /usr/lib/librewolf/native-messaging-hosts/org.kde.plasma.browser_integration.json ]]
+
+echo Successfully applied.
