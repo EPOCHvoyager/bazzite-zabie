30fe3829884f4900fc9bcd088ea8141781e0055d
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-08-07 19:17:57 -0300

    Reapply "Remove ananicy-cpp"
    This reverts commit d7b28f870aad77c9db8b61078a77f183f81ee0e1.

--- build_files/packages/copr.sh
+++ build_files/packages/copr.sh
@@ -19,23 +19,6 @@ _get_from_copr () {
 
 echo Installing packages from Copr…
 
-# This Copr repository is included in the base image. Thus, enable it ephemerally with --enable-repo, passing the repo ID.
-COPR="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons"
-dnf5 -y install \
-	--setopt=tsflags=noscripts \
-	https://download.copr.fedorainfracloud.org/results/bieszczaders/kernel-cachyos-addons/fedora-43-x86_64/10428026-ananicy-cpp/ananicy-cpp-1.2.0-1.fc43.x86_64.rpm
-# TODO: Remove once there's a new successful build on Copr
-dnf5 -y install \
-	--enable-repo="${COPR}" \
-	cachyos-ananicy-rules
-	#ananicy-cpp
-
-
-rpm -V \
-    ananicy-cpp \
-    cachyos-ananicy-rules
-dnf5 repolist --disabled | grep -q "${COPR}"
-
 # This package needs to be rebuilt for specific versions of Plasma.
 COPR="infinality/kwin-effects-better-blur-dx"
 PACKAGES=( "kwin-effects-better-blur-dx" )
@@ -56,11 +39,9 @@ echo Successfully installed.
 
 echo Enabling service units…
 
-systemctl enable ananicy-cpp.service
 systemctl enable coolercontrold.service
 
 
-systemctl is-enabled ananicy-cpp.service
 systemctl is-enabled coolercontrold.service
 
 echo Successfully enabled.
