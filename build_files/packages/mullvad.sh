5faf544c3410af91b0c2e6f68a3485262ac15390
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-08-26 17:45:46 -0300

    Revert "Remove Mullvad for testing"
    This reverts commit 65d3634a7bdfe3620099a73823366d215d1aeee8.

---
+++ build_files/packages/mullvad.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set ${CI:+-x} -euo pipefail
+
+echo Installing Mullvad VPN software…
+
+REPO_URL="https://repository.mullvad.net/rpm/stable/mullvad.repo"
+PACKAGE="mullvad-vpn"
+REPO_ID="mullvad-stable"
+
+dnf5 config-manager addrepo \
+	--from-repofile="${REPO_URL}"
+dnf5 -y install \
+	--setopt=tsflags=noscripts \
+	"${PACKAGE}"
+dnf5 config-manager disable \
+	"${REPO_ID}"
+
+
+rpm -V \
+    "${PACKAGE}"
+dnf5 repolist --disabled | grep -q "${REPO_ID}"
+
+echo Successfully installed.
+
+
+echo Adding permissions…
+
+# This is normally handled by an install scriptlet.
+chmod u+s "/usr/bin/mullvad-exclude"
+
+
+[[ $( stat --format='%a' /usr/bin/mullvad-exclude ) = "4755" ]]
+
+echo Successfully added.
+
+
+echo Enabling service units…
+
+systemctl enable mullvad-daemon.service
+systemctl enable mullvad-early-boot-blocking.service
+
+
+systemctl is-enabled mullvad-daemon.service
+systemctl is-enabled mullvad-early-boot-blocking.service
+
+echo Successfully enabled.
