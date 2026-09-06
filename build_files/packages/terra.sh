65161a9889123b9f7d700a21dd24e9d8ea3cdcbe
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-09-06 09:31:29 -0300

    Add system76-scheduler
    - For testing purposes.

---
+++ build_files/packages/terra.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set ${CI:+-x} -euo pipefail
+
+PACKAGES=( "system76-scheduler" )
+UNITS=( "com.system76.Scheduler.service" )
+
+_install() {
+    dnf5 -y install \
+        --enable-repo="terra" \
+        "${PACKAGES[@]}"
+
+
+    rpm -V \
+        "${PACKAGES[@]}"
+}
+
+_unit_setup() {
+    for u in "${UNITS[@]}"; do
+        systemctl enable "$u" || exit 1
+
+
+        systemctl is-enabled "$u" || exit 1
+    done
+}
+
+echo Installing package from Terra…
+
+_install
+
+echo Successfully installed.
+
+echo Enabling service unit…
+
+_unit_setup
+
+echo Successfully enabled.
