274131124cacfc1f7e128acb452ab74532e34968
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-09-06 11:12:40 -0300

    Move path into array

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,11 +4,11 @@ set ${CI:+-x} -euo pipefail
 
 UTILITY_DIR="/ctx/utilities"
 UTILITY_SCRIPTS=( \
-"copy-files.sh" \
-"enable-pci-latency.sh" \
-"install-packages.sh" \
-"remove-gamemode-config.sh" )
+"${UTILITY_DIR}/copy-files.sh" \
+"${UTILITY_DIR}/enable-pci-latency.sh" \
+"${UTILITY_DIR}/install-packages.sh" \
+"${UTILITY_DIR}/remove-gamemode-config.sh" )
 
-for s in "${UTILITY_DIR}/${UTILITY_SCRIPTS[@]}"; do
+for s in "${UTILITY_SCRIPTS[@]}"; do
 	sh -c "$s" || exit 1
 done
