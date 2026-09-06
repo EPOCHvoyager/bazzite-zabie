97c59ec44efe10a6807579996ff29f625a64c874
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-09-06 10:21:12 -0300

    Reduce white space usage in main script

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,17 +7,14 @@ cp -avf "/ctx/system_files"/. /
 
 # Enable local service unit from system_files.
 echo Enabling pci-latency service…
-
 systemctl enable pci-latency.service
 
 
 systemctl is-enabled pci-latency.service
-
 echo Successfully enabled.
 
 # Run all package installation scripts in set directory.
 echo Installing packages…
-
 PACKAGE_DIR="/ctx/packages"
 shopt -s nullglob ; scripts=("${PACKAGE_DIR}"/*.sh) ; SCRIPT_COUNT="${#scripts[@]}" ; shopt -u nullglob
 SCRIPTS_RAN=0
@@ -28,15 +25,12 @@ done
 
 
 [[ "${SCRIPTS_RAN}" == "${SCRIPT_COUNT}" ]]
-
 echo Package installation done.
 
 # Remove stock settings for user scripting-only use.
 echo Removing Feral gamemode stock configuration…
-
 rm /usr/share/gamemode/gamemode.ini
 
 
 [[ ! -f "/usr/share/gamemode/gamemode.ini" ]]
-
 echo Successfully removed.
